45d4236faba6550438cd81a83e5210d16382df87
saif <mohamedhawas123@gmail.com>
2020-12-14 12:30:58 +0200

    comments

--- blogg/views.py
+++ blogg/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .form import EmailPostForm
+from .form import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 def post_list(request):
@@ -26,22 +26,40 @@ def post_detail(request, year, month, day, post):
      publish__month = month,
      publish__day = day
     )
-    return render(request, 'blogg/post/detail.html', {'post': posts})
+
+    comments = posts.comments.filter(active = True)
+    new_comment = None
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post=posts
+            new_comment.save()
+
+    else:
+        comment_form = CommentForm()
+
+
+    return render(request, 'blogg/post/detail.html', {'post': posts, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
 
 
 def post_share(request, post_id):
+    sent = False
     post = get_object_or_404(Post, id=post_id, status='draft')
     if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            post_url = request.build_aboslute_uri(post.get_absolute_url())
-            subject = f"{cd['name']} recommend you read" f"{cd['post.ttile']}"
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommend you read" f"{post.title}"
+            message = f" Read {post.title} at {post_url}\n\n"
+            send_mail(subject, message, 'mohamedhawas123@gmail.com', [cd['to']])
+            sent= True
 
             
 
     else:
         form = EmailPostForm()
     
-    return render(request, 'blogg/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blogg/post/share.html', {'post': post, 'form': form, 'sent': sent})
            
